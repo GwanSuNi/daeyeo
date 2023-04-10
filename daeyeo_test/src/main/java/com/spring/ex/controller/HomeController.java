109244dd2becc799dc31829f7ec1e223225b2142
GwanSuNi <tony2yoo@gmail.com>
2023-04-10 16:20:49 +0900

    [add] Init project
    테스트 프로젝트 초기화

---
+++ daeyeo_test/src/main/java/com/spring/ex/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.spring.ex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
