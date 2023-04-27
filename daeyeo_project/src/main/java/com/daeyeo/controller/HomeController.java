a806412bbee0c1ef72f998c92be77d6302706bcc
GwanSuNi <tony2yoo@gmail.com>
2023-04-27 17:23:36 +0900

    [add] 프로젝트 기본 템플릿

---
+++ daeyeo_project/src/main/java/com/daeyeo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.daeyeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
