ca4bd186f81d4735f5f385d5c1ef66b2e0fa1e8f
GwanSuNi <tony2yoo@gmail.com>
2023-05-18 16:22:36 +0900

    [add] LoginController 추가, HomeController 수정

---
+++ daeyeo_project/src/main/java/com/daeyeo/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.daeyeo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @RequestMapping("")
+    public String loginPage() {
+        return "login/member_login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {return "login/member_register";}
+}
---
+++ daeyeo_project/src/main/webapp/WEB-INF/views/login/member_register.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: gwansuni
+  Date: 2023/05/16
+  Time: 20:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
