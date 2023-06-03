9697735961a861eac56b206541db183262241b4a
GwanSuNi <tony2yoo@gmail.com>
2023-06-03 16:13:51 +0900

    [update] UserEntity와 BanLog 양방향 매핑
    컬렉션 타입 List로 변경까지

--- daeyeo_project/src/main/java/com/daeyeo/entity/BanLog.java
+++ daeyeo_project/src/main/java/com/daeyeo/entity/BanLog.java
@@ -1,8 +1,7 @@
 package com.daeyeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -10,25 +9,32 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@ToString(exclude = {"userEntity"})
 @NoArgsConstructor
 @AllArgsConstructor
+@DynamicUpdate
 @Table(name="Ban_Log")
 public class BanLog {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "banId")
     private int banId;
+    @ManyToOne
+    @JoinColumn(name = "userEmail")
+    private UserEntity userEntity;
     private boolean flag;
     private String banReason;
     private LocalDateTime duration;
     private LocalDateTime banDate;
 
-    public void setBanFlagTrue() {
-        flag = true;
-    }
-    public void setBanFlagFalse() {
-        flag = false;
-    }
+    public void switchBanFlag(boolean flag) { this.setFlag(flag);}
 
+    /**
+     *
+     * @param flag
+     * @param banReason
+     * @param duration
+     */
     public BanLog(boolean flag, String banReason, LocalDateTime duration) {
         this.flag = flag;
         this.banReason = banReason;
--- daeyeo_project/src/main/java/com/daeyeo/entity/UserEntity.java
+++ daeyeo_project/src/main/java/com/daeyeo/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package com.daeyeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.Fetch;
 
@@ -13,28 +10,28 @@ import java.util.*;
 
 @Entity
 @Data
+@ToString(exclude = {"banLog"})
 //@DynamicUpdate
 @Table(name = "User")
-@EqualsAndHashCode(exclude = {"rentalObjects", "rentalLogs"})
+@EqualsAndHashCode(exclude = {"rentalObjects", "rentalLogs", "banLogs"})
 @NoArgsConstructor
 @SecondaryTables({
         @SecondaryTable(name = "Report_Log",
                 pkJoinColumns = @PrimaryKeyJoinColumn(name = "userEmail", referencedColumnName = "userEmail")
-        ),
-        @SecondaryTable(name = "Ban_Log",
-                pkJoinColumns = @PrimaryKeyJoinColumn(name = "userEmail", referencedColumnName = "userEmail")
-        )}
-)
+        )
+})
 public class UserEntity {
     public UserEntity(String userEmail) {
         this.userEmail = userEmail;
     }
+
     @Id
     private String userEmail;
     private String userPw;
     private String userName;
     private String statusMsg;
-    private String location;
+    @Embedded
+    private Address location;
     private String phoneNum;
     private String department;
     private String userCategory;
@@ -42,47 +39,53 @@ public class UserEntity {
     private LocalDateTime registDate;
 
     // Embedded 였는데 수정
-    @ElementCollection (fetch = FetchType.EAGER)
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(
             name = "User_Memo",
-            joinColumns = @JoinColumn(name ="userEmail")
+            joinColumns = @JoinColumn(name = "userEmail")
     )
     @Column(name = "userMemo")
 //    @OrderColumn(name = "memoId")
     private Set<UserMemo> userMemo;
     @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable (
+    @CollectionTable(
             name = "Report_Log",
             joinColumns = @JoinColumn(name = "userEmail")
     )
     private Set<ReportLog> reportLog;
-//    @ElementCollection(fetch = FetchType.EAGER)
+    //    @ElementCollection(fetch = FetchType.EAGER)
 //    @CollectionTable(
 //            name = "Ban_Log",
 //            joinColumns = @JoinColumn(name = "userEmail")
 //    )
 //    @OrderColumn(name = "banId")
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinColumn(name = "userEmail")
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "userEntity")
     @OrderColumn(name = "banId")
-    private List<BanLog> banLog; // TODO: List로 변환
+    private List<BanLog> banLogs = new ArrayList<>(); // TODO: List로 변환
+
+    public void addBanLog(BanLog banLog) {
+        this.getBanLogs().add(banLog);
+    }
+
     private int paySum;
     private int commissionSum;
     private int rate;
     private boolean quitFlag;
 
-    @OneToMany(fetch = FetchType.EAGER , cascade = CascadeType.ALL, mappedBy = "userEntity")
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "userEntity")
     private Set<RentalObject> rentalObjects = new HashSet<>();
-    public void addRentalObject(RentalObject rentalObject){
+
+    public void addRentalObject(RentalObject rentalObject) {
         this.getRentalObjects().add(rentalObject);
     }
 
 
-    @OneToMany (fetch = FetchType.EAGER , cascade = CascadeType.ALL, mappedBy = "userEntity")
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "userEntity")
     private Set<RentalLog> rentalLogs = new HashSet<>();
-    public void addRentalLog(RentalLog rentalLog) { this.getRentalLogs().add(rentalLog);  }
 
-
+    public void addRentalLog(RentalLog rentalLog) {
+        this.getRentalLogs().add(rentalLog);
+    }
 
 
     @ElementCollection(fetch = FetchType.EAGER)
@@ -95,16 +98,14 @@ public class UserEntity {
     private Map<String, String> wishLists = new HashMap();
 
 
-
-
     @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "writer")
     private Set<Review> reviews = new HashSet<>();
 
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(
-            name ="Advertisement",
-            joinColumns = @JoinColumn(name ="adOwnerEmail")
+            name = "Advertisement",
+            joinColumns = @JoinColumn(name = "adOwnerEmail")
     )
     @Column(name = "advertisement")
     private Set<Advertisement> advertisement = new HashSet<>();
