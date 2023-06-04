e267f44cb36edcc7995632daf4e2435931acd0c4
GwanSuNi <tony2yoo@gmail.com>
2023-06-04 15:37:42 +0900

    [update] Address 임베더블 수정

--- daeyeo_project/src/main/java/com/daeyeo/entity/Address.java
+++ daeyeo_project/src/main/java/com/daeyeo/entity/Address.java
@@ -1,11 +1,15 @@
 package com.daeyeo.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import javax.persistence.Id;
 
 @Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Address {
     private int zipCode;
@@ -13,4 +17,8 @@ public class Address {
     private String jibunAddress;
     private String sido;
     private String sigungu;
+
+    public Address(int zipCode) {
+        this.zipCode = zipCode;
+    }
 }
