9a0a7aa23db0696c29b60c0f06899cf436e41655
Ryuintae <dlsxichlrg@naver.com>
2023-06-08 11:24:50 +0900

    [add] 로그인 완성
    revert한 로그인 다시 커밋

---
+++ daeyeo_project/src/main/java/com/daeyeo/exception/NotCorrespondingEmailException.java
@@ -0,0 +1,10 @@
+package com.daeyeo.exception;
+
+public class NotCorrespondingEmailException extends RuntimeException {
+
+
+    public NotCorrespondingEmailException(String message) {
+        super(message);
+    }
+
+}
