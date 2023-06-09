6a495cc5134125fce99b599ace0d6e5cbe4da24b
tecte <tecte1023@gmail.com>
2023-06-09 12:05:21 +0900

    [update] rentalList에 rentalObject를 뿌리기 위한 jps, js, css 파일 수정

--- daeyeo_project/src/main/webapp/resources/js/header.js
+++ daeyeo_project/src/main/webapp/resources/js/header.js
@@ -2,5 +2,17 @@ const toggleBtn = document.querySelector('#bar_li');
 const menu = document.querySelector('.navbar_menu');
 
 toggleBtn.addEventListener('click', () => {
-   menu.classList.toggle('active');
+    menu.classList.toggle('active');
+});
+
+const hCategories = document.querySelectorAll('.header-cate');
+const hForm = document.querySelector('#header-form');
+const hMainCate = document.querySelector('.header-main-cate');
+
+hCategories.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+        hMainCate.value = element.innerText;
+        hForm.submit();
+    });
 });
--- daeyeo_project/src/main/webapp/resources/js/rental/rentalList.js
+++ daeyeo_project/src/main/webapp/resources/js/rental/rentalList.js
@@ -5,7 +5,7 @@ btn.forEach((element) => {
     element.addEventListener('click', () => {
         let sibling = element.nextElementSibling;
 
-        if(!sibling)
+        if (!sibling)
             sibling = element.previousElementSibling;
 
         element.classList.add('active');
@@ -13,3 +13,48 @@ btn.forEach((element) => {
         rentalList.className = element.id;
     });
 });
+
+const url = new URL(window.location.href);
+const params = url.searchParams;
+const cateForm = document.querySelector('.category_wrap')
+let mainCate = params.get('mainCate');
+let subCate = params.get('subCate');
+const firstCateBtn = document.querySelector('.cate-btn:first-child');
+const cateBtn = document.querySelectorAll('.cate-btn:not(:first-child)');
+
+firstCateBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    if (mainCate !== '')
+        params.set('subCate', '');
+
+    // cateForm.submit();
+    url.search = params.toString();
+    window.location.replace(url.toString());
+});
+
+cateBtn.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        // TODO : 뒤로가기 시 데이터가 남아있어서 뒤로갔다 클릭하면 문제 생김
+        // 예를 들어 list?searchWord=&mainCate=공간시설&subCate= 이 되야 하는데 뒤로 갔다 클릭하면 list?searchWord=&mainCate=공간시설&subCate=공간시설이 됨
+        if (mainCate === '')
+            params.set('mainCate', element.innerText);
+        else
+            params.set('subCate', element.innerText);
+
+        // cateForm.submit();
+        url.search = params.toString();
+        window.location.replace(url.toString());
+    });
+});
+
+if (mainCate === '' || subCate === '')
+    firstCateBtn.classList.add('active');
+else {
+    cateBtn.forEach((element) => {
+        if (element.innerText === subCate)
+            element.classList.add('active');
+    });
+}
