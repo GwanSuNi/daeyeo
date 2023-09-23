26945e88af053f0a737c574d2520d67628cb5c86
GwanSuNi <tony2yoo@gmail.com>
2023-09-24 01:28:36 +0900

    [update] Member 엔티티 생성자 수정

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/entity/Member.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/entity/Member.java
@@ -4,11 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Entity
 public class Member {
+    public Member(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
