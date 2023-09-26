c95c5934a09402913d2a1eeeb7aaef724d136b38
GwanSuNi <tony2yoo@gmail.com>
2023-09-27 00:03:49 +0900

    [add] 유저 회원가입 시 컨트롤러
    엑세스토큰과 리프레시토큰 발급

---
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/controller/UserController.java
@@ -0,0 +1,52 @@
+package com.daeyeo.helloDaeyeo.controller;
+
+import com.daeyeo.helloDaeyeo.config.jwt.TokenProvider;
+import com.daeyeo.helloDaeyeo.dto.AddUserRequest;
+import com.daeyeo.helloDaeyeo.dto.CreateAccessTokenResponse;
+import com.daeyeo.helloDaeyeo.entity.RefreshToken;
+import com.daeyeo.helloDaeyeo.entity.User;
+import com.daeyeo.helloDaeyeo.service.RefreshTokenService;
+import com.daeyeo.helloDaeyeo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+
+// 새로 만들어봄
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    // TODO: 만료기간 변수화
+    // 매개변수에 원래 @RequestBody가 있었지만
+    // Content type 'application/x-www-form-urlencoded;charset=UTF-8' not supported 에러 때문에 뺌
+    @PostMapping("/user")
+    public ResponseEntity<CreateAccessTokenResponse> signup(AddUserRequest request) {
+        // 요청하는 계정으로 가입하고 성공 시 User 클래스로 반환
+        User newUser = userService.findById(userService.save(request));
+
+        // 엑세스 토큰과 리프레시 토큰 발급
+        // TODO: 토큰 만료 테스트
+        String accessToken = tokenProvider.generateToken(newUser, Duration.ofSeconds(600000));
+        // 리프레시 토큰 서버에 저장
+        RefreshToken refresh = refreshTokenService.createRefreshToken(newUser);
+
+        // 엑세스 토큰과 리프레시 토큰을 클라이언트에 반환
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(accessToken, refresh.getRefreshToken()));
+    }
+}
