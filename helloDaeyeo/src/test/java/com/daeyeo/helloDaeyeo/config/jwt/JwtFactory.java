97d47dc52963c37b25cc78874fddccf0df10197c
GwanSuNi <tony2yoo@gmail.com>
2023-09-27 00:04:53 +0900

    [add] TokenProvider와 TokenApiController 테스트 코드

---
+++ helloDaeyeo/src/test/java/com/daeyeo/helloDaeyeo/config/jwt/JwtFactory.java
@@ -0,0 +1,48 @@
+package com.daeyeo.helloDaeyeo.config.jwt;
+
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+@Getter
+public class JwtFactory {
+    private String subject = "test@test.com";
+    private Date issuedAt = new Date();
+    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+    private Map<String, Object> claims = emptyMap();
+
+    // 빌더 패턴을 사용해 설정이 필요한 데이터만 선택 설정
+    @Builder
+    public JwtFactory(String subject, Date issuedAt, Date expiration, Map<String, Object> claims) {
+        this.subject = subject != null ? subject : this.subject;
+        this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
+        this.expiration = expiration != null ? expiration : this.expiration;
+        this.claims = claims != null ? claims : this.claims;
+    }
+
+    public static JwtFactory withDefaultValues() {
+        return JwtFactory.builder().build();
+    }
+
+    // jjwt 라이브러리를 사용해 JWT 토큰 생성
+    public String createToken(JwtProperties jwtProperties) {
+        return Jwts.builder()
+                .setSubject(subject)
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(jwtProperties.getIssuer())
+                .setIssuedAt(issuedAt)
+                .setExpiration(expiration)
+                .addClaims(claims)
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .compact();
+    }
+}
