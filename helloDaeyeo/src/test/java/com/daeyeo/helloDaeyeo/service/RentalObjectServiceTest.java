f1dee7af8a8eb16042c57a8504a7a142fe6f0ee5
tecte <tecte1023@gmail.com>
2023-10-13 01:15:46 +0900

    [update] rentalObjectDto 수정 및 위치 변경, rentalObjectMapper 수정

--- helloDaeyeo/src/test/java/com/daeyeo/helloDaeyeo/service/RentalObjectServiceTest.java
+++ helloDaeyeo/src/test/java/com/daeyeo/helloDaeyeo/service/RentalObjectServiceTest.java
@@ -1,6 +1,5 @@
 package com.daeyeo.helloDaeyeo.service;
 
-import com.daeyeo.helloDaeyeo.dto.RentalObjectDto;
 import com.daeyeo.helloDaeyeo.entity.Member;
 import org.junit.Test;
 import org.junit.platform.commons.logging.Logger;
