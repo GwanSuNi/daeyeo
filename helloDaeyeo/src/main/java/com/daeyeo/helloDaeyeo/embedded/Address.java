3a6ace6481a62ac008c7e0b9a93089c26d81a496
tecte <tecte1023@gmail.com>
2023-10-12 17:03:01 +0900

    [update] embedded 수정

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/embedded/Address.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/embedded/Address.java
@@ -1,6 +1,18 @@
 package com.daeyeo.helloDaeyeo.embedded;
 
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Embeddable
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
 public class Address {
-
+    private String roadName;
+    private String regionName;
+    private String sido;
+    private String sigungu;
+    private String zipCode;
 }
