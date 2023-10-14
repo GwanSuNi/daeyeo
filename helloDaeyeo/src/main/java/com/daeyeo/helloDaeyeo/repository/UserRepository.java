1aa979702c782bae6c07743e912ff68ea51aff2d
GwanSuNi <tony2yoo@gmail.com>
2023-10-14 17:18:18 +0900

    [update] 안쓰는 엔티티 관련 클래스들 주석처리

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/repository/UserRepository.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/repository/UserRepository.java
@@ -1,10 +1,10 @@
-package com.daeyeo.helloDaeyeo.repository;
-
-import com.daeyeo.helloDaeyeo.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository  extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
+//package com.daeyeo.helloDaeyeo.repository;
+//
+//import com.daeyeo.helloDaeyeo.entity.User;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.Optional;
+//
+//public interface UserRepository  extends JpaRepository<User, Long> {
+//    Optional<User> findByEmail(String email);
+//}
