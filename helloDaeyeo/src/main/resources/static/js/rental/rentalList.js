ff38755830a6a223ad9d4ffca819fdf7fbf65210
tecte <tecte1023@gmail.com>
2023-10-17 01:10:30 +0900

    [update] html, js 수정

--- helloDaeyeo/src/main/resources/static/js/rental/rentalList.js
+++ helloDaeyeo/src/main/resources/static/js/rental/rentalList.js
@@ -1,62 +1,39 @@
-const btn = document.querySelectorAll('.btn');
-const rentalList = document.querySelector('#rental-list');
+const listType = document.getElementById('list-type');
+const boardType = document.getElementById('board-type');
 
-btn.forEach((element) => {
-    element.addEventListener('click', () => {
-        let sibling = element.nextElementSibling;
+// 목록형, 보드형 클릭 시 typeActive 호출
+listType.addEventListener('click', (e) => typeActive(e.currentTarget));
+boardType.addEventListener('click', (e) => typeActive(e.currentTarget));
 
-        if (!sibling)
-            sibling = element.previousElementSibling;
-
-        element.classList.add('active');
-        sibling.classList.remove('active');
-        rentalList.className = element.id;
-    });
-});
-
-const url = new URL(window.location.href);
-const params = url.searchParams;
-const cateForm = document.querySelector('.category_wrap')
-let mainCate = params.get('mainCate');
-let subCate = params.get('subCate');
-const firstCateBtn = document.querySelector('.cate-btn:first-child');
-const cateBtn = document.querySelectorAll('.cate-btn:not(:first-child)');
-
-firstCateBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-
-    if (mainCate !== '')
-        params.set('subCate', '');
-
-    // cateForm.submit();
-    url.search = params.toString();
-    window.location.replace(url.toString());
-});
-
-cateBtn.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        if (mainCate === '' || mainCate === null)
-            params.set('mainCate', element.innerText);
-        else
-            params.set('subCate', element.innerText);
-
-        // cateForm.submit();
-        url.search = params.toString();
-        window.location.replace(url.toString());
-    });
-});
-
-if (mainCate === '' || subCate === '' || mainCate === null || subCate === null)
-    firstCateBtn.classList.add('active');
-else {
-    cateBtn.forEach((element) => {
-        if (element.innerText === subCate)
-            element.classList.add('active');
-    });
+// 목록형, 보드형 바꿔주는 함수
+function typeActive(type) {
+    listType.classList.remove('active');
+    boardType.classList.remove('active');
+    type.classList.add('active');
+    document.getElementById('rental-list').className = type.id;
+    sessionStorage.setItem("type", type.id);
 }
 
+// 페이지 로드 시
+window.addEventListener('load', () => {
+    let type = sessionStorage.getItem("type");
+
+    if (type) // 세션 스토리지에서 가져온 id 값이 있으면 요소 찾기
+        typeActive(document.getElementById(type));
+});
+
+const categories = document.querySelectorAll('.cate-btn');
+const mainCategory = document.getElementById('mainCategory');
+const subCategory = document.getElementById('subCategory');
+let category = mainCategory.value ? mainCategory : subCategory;
+
+// 카테고리에 클릭 이벤트 추가
+categories.forEach((element) => {
+    element.addEventListener('click', (e) => {
+        e.preventDefault();
+
+    });
+});
 
 const rentalObjectForm = document.querySelector('.object-form');
 const rentalObjects = document.querySelectorAll('.rental-object');
@@ -64,7 +41,7 @@ const rentalObjects = document.querySelectorAll('.rental-object');
 rentalObjects.forEach((element) => {
     element.addEventListener('click', (event) => {
         event.preventDefault();
-        element.previousElementSibling.setAttribute('name','objectId');
+        element.previousElementSibling.setAttribute('name', 'objectId');
         rentalObjectForm.submit();
     });
 });
