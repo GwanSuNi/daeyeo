809a8fe95643819a6d7bb6e5b61297f677a71399
tecte <tecte1023@gmail.com>
2023-10-17 01:18:09 +0900

    [update] 에러 헨들러, RentalObjectMapper 수정

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/mapper/RentalObjectMapper.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/mapper/RentalObjectMapper.java
@@ -5,7 +5,6 @@ import com.daeyeo.helloDaeyeo.dto.rental.RentalRegisterDto;
 import com.daeyeo.helloDaeyeo.entity.Member;
 import com.daeyeo.helloDaeyeo.entity.RentalObject;
 import com.daeyeo.helloDaeyeo.entity.SubCategory;
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -13,11 +12,12 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RentalObjectMapper {
+    @Mapping(source = "subCategory.mainCategory.mcId", target = "mcId")
     @Mapping(source = "subCategory.scId", target = "scId")
     @Mapping(source = "member.userEmail", target = "userEmail")
     RentalObjectDto toDto(RentalObject rentalObject);
 
-    RentalObject toEntity(RentalRegisterDto rentalRegisterDto, @Context SubCategory subCategory, @Context Member member);
+    RentalObject toEntity(RentalRegisterDto rentalRegisterDto, SubCategory subCategory, Member member);
 
     List<RentalObjectDto> toDtoList(List<RentalObject> rentalObjects);
 }
