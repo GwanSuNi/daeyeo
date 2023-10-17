235b77a2d048a04926c6c97e9e8a22971c1ae05a
GwanSuNi <tony2yoo@gmail.com>
2023-10-18 05:58:22 +0900

    [update] 시큐리티 설정 업그레이드

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/config/WebSecurityConfig.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/config/WebSecurityConfig.java
@@ -5,13 +5,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
@@ -29,27 +29,55 @@ public class WebSecurityConfig {
         return (web) -> web.ignoring()
                 .requestMatchers(toH2Console())
                 .requestMatchers("/static/**", "/css/**", "/error", "/swagger-ui/**", "/v3/**",
-                                "/js/**", "/img/**");
+                        "/js/**", "/img/**");
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-//                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .requestMatchers( "/user","/adminpage/**","/myPage/**","/login/**", "/memberApi/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
+//        return http
+                http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/user", "/login/**", "/memberApi/**", "/loginCheck").permitAll()
+                                .requestMatchers("/memberApi/memberLogin").anonymous()
+                                .requestMatchers("/memberApi/register").anonymous()
+                                .requestMatchers("/myPage/**").hasAnyRole("MEMBER", "ADMIN")
+                                .requestMatchers("/adminpage/**").hasRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                                .usernameParameter("username")
+                .passwordParameter("password")
                 .defaultSuccessUrl("/", true)
-                .and()
-                .logout()
-                .logoutSuccessUrl("/login")
-                .invalidateHttpSession(true)
-                .and()
-                .csrf().disable()
-                .build();
+                )
+                .logout((logout) -> logout.permitAll())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+
+        // 옛날 방식 짬뽕이라 주석처리
+//                .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+//                .authorizeRequests(authorizeRequests ->
+//                        authorizeRequests
+//                                .requestMatchers("/user", "/login/**", "/memberApi/**", "/loginCheck").permitAll()
+//                                .requestMatchers("/memberApi/memberLogin").anonymous()
+//                                .requestMatchers("/memberApi/register").anonymous()
+//                                .requestMatchers("/myPage/**").hasAnyRole("MEMBER", "ADMIN")
+//                                .requestMatchers("/adminpage/**").hasRole("ADMIN")
+//                                .anyRequest().authenticated()
+//                )
+//                .formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/", true)
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login")
+//                .invalidateHttpSession(true)
+//                .deleteCookies("true")
+//                .and()
+//                .csrf().disable()
+//                .build();
     }
 
     @Bean
