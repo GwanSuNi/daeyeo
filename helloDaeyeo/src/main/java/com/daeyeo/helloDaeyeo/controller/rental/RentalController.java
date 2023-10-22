04911689ef17d7488e3eefefe2915db694b07ad5
tecte <tecte1023@gmail.com>
2023-10-23 02:30:52 +0900

    [update] rentalList todo 추가

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/controller/rental/RentalController.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/controller/rental/RentalController.java
@@ -3,15 +3,9 @@ package com.daeyeo.helloDaeyeo.controller.rental;
 import com.daeyeo.helloDaeyeo.dto.category.MainCategoryDto;
 import com.daeyeo.helloDaeyeo.dto.category.SubCategoryDto;
 import com.daeyeo.helloDaeyeo.dto.rental.*;
-import com.daeyeo.helloDaeyeo.embedded.Address;
-import com.daeyeo.helloDaeyeo.entity.RentalObject;
-import com.daeyeo.helloDaeyeo.entity.SubCategory;
-import com.daeyeo.helloDaeyeo.repository.MemberRepository;
 import com.daeyeo.helloDaeyeo.service.MainCategoryService;
-import com.daeyeo.helloDaeyeo.service.MemberService;
 import com.daeyeo.helloDaeyeo.service.RentalObjectService;
 import com.daeyeo.helloDaeyeo.service.SubCategoryService;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -26,23 +20,21 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/rental")
+@RequestMapping("/rentals")
 public class RentalController {
     private final SubCategoryService subCategoryService;
     private final RentalObjectService rentalObjectService;
-    private final MemberService memberService;
-    private final MemberRepository memberRepository;
     private final MainCategoryService mainCategoryService;
 
 
     // TODO: 지금은 db에서 데이터들을 다 가져온다음 페이징을 하는 데 db에서 페이징을 한 데이터들을 가져오는 걸로 바꿔야 함
+    // TODO: rentalObject가 하나도 없을 때 '검색 결과가 없습니다.'라고 보여주고 html에 페이징도 안 보이게 해야함
     @GetMapping({"list", "list/{page}"})
     public String rentalList(SearchSpecDto specDto, @PathVariable(required = false) Integer page, Model model) {
         if (page == null)
             page = 1;
 
         Pageable pageable = PageRequest.of(page - 1, 9);
-
         List<String> categories = subCategoryService.getCategories(specDto.getMainCategory());
         Page<RentalObjectDto> rentalObjectDtos = rentalObjectService.findListBySearchSpec(specDto, pageable);
 
@@ -59,11 +51,13 @@ public class RentalController {
         model.addAttribute("rentalStatus", new RentalStatusFormDto());
         return "rental/rentalWrite";
     }
+
     @PostMapping("write.do")
-    public String rentalStatusSend(@ModelAttribute("rentalStatus")RentalStatusFormDto rentalStatusFormDto){
+    public String rentalStatusSend(@ModelAttribute("rentalStatus") RentalStatusFormDto rentalStatusFormDto) {
 
         return "rental/rentalWrite";
     }
+
     @GetMapping("/getSubCategories")
     @ResponseBody
     public List<SubCategoryDto> getSubCategories(@RequestParam("mainCategoryId") String mainCategoryId) {
@@ -77,18 +71,17 @@ public class RentalController {
 //      memberService.validateMember(request);
         List<MainCategoryDto> mainCategoryList = mainCategoryService.getAllCategories();
         model.addAttribute("rentalRegister", new RentalRegisterFormDto());
-        model.addAttribute("mainCategoryList",mainCategoryList);
+        model.addAttribute("mainCategoryList", mainCategoryList);
         return "rental/rentalRegistrationForm";
     }
 
 
-
     @PostMapping("rentalRegistrationForm")
-    public String register(@Valid @ModelAttribute("rentalRegister")RentalRegisterFormDto rentalRegisterFormDto , BindingResult bindingResult,Model model) {
-        if(bindingResult.hasErrors()){
+    public String register(@Valid @ModelAttribute("rentalRegister") RentalRegisterFormDto rentalRegisterFormDto, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             return "rental/rentalRegistrationForm";
-        }else if (rentalRegisterFormDto.getScId()==null){
-            model.addAttribute("scIdChoice","장소를 선택해주세요");
+        } else if (rentalRegisterFormDto.getScId() == null) {
+            model.addAttribute("scIdChoice", "장소를 선택해주세요");
             return "rental/rentalRegistrationForm";
         }
         RentalRegisterDto rentalRegisterDto = new RentalRegisterDto(rentalRegisterFormDto);
