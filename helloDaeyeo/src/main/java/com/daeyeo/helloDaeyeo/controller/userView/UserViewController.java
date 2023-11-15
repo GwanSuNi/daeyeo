dcbf98c21b1551b457e3c32058c374e0856a8679
tecte <tecte1023@gmail.com>
2023-11-15 17:11:51 +0900

    [update] 메인페이지에서 최근 사용 서비스와 나의 예약 정보 나오게 controller, service, reopository 수정

--- helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/controller/userView/UserViewController.java
+++ helloDaeyeo/src/main/java/com/daeyeo/helloDaeyeo/controller/userView/UserViewController.java
@@ -1,13 +1,10 @@
 package com.daeyeo.helloDaeyeo.controller.userView;
 
-import com.daeyeo.helloDaeyeo.dto.category.MainCategoryDto;
-import com.daeyeo.helloDaeyeo.dto.category.SubCategoryDto;
-import com.daeyeo.helloDaeyeo.dto.rental.RentalObjectDto;
+import com.daeyeo.helloDaeyeo.dto.mainPage.MainPageRentalItemDto;
+import com.daeyeo.helloDaeyeo.dto.mainPage.MainPageResponseDto;
 import com.daeyeo.helloDaeyeo.dto.rental.RentalStatusDto;
-import com.daeyeo.helloDaeyeo.service.MainCategoryService;
-import com.daeyeo.helloDaeyeo.service.RentalObjectService;
-import com.daeyeo.helloDaeyeo.service.RentalStatusService;
-import com.daeyeo.helloDaeyeo.service.SubCategoryService;
+import com.daeyeo.helloDaeyeo.mapper.MainPageMapper;
+import com.daeyeo.helloDaeyeo.service.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -17,10 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.*;
 
 @Controller
 @Slf4j
@@ -30,6 +24,8 @@ public class UserViewController {
     private final SubCategoryService subCategoryService;
     private final RentalStatusService rentalStatusService;
     private final RentalObjectService rentalObjectService;
+    private final MainPageService mainPageService;
+    private final MainPageMapper mainPageMapper;
 
     @GetMapping("/login")
     public String login() {
@@ -45,23 +41,12 @@ public class UserViewController {
     public String mainPage(Model model, @CurrentSecurityContext(expression = "authentication") Authentication authentication) {
         log.info("principal : {}, name: {}, authorities: {}, details : {}", authentication.getPrincipal(), authentication.getName(), authentication.getAuthorities(), authentication.getDetails());
 
-        List<MainCategoryDto> mainCategories = mainCategoryService.getAllCategories();
-        Map<String, List<String>> categories = new LinkedHashMap<>();
-
-        for (MainCategoryDto mainCategory : mainCategories) {
-            List<SubCategoryDto> subCategories = subCategoryService.getSubCategories(mainCategory.getMcId());
-            categories.put(mainCategory.getMcId(), subCategories.stream().map(SubCategoryDto::getScId).collect(Collectors.toList()));
-        }
-
-        List<RentalStatusDto> rentalStatusDtos = rentalStatusService.findRentalStatuses(authentication.getName());
-        List<RentalObjectDto> rentalObjectDtos = rentalObjectService.getRentalObjects(rentalStatusDtos);
-
-        model.addAttribute("categories", categories);
-        model.addAttribute("rentalStatuses", rentalStatusDtos);
-        model.addAttribute("rentalObjects", rentalObjectDtos);
+        MainPageResponseDto mainPageResponseDto = mainPageService.prepareMainPage(authentication.getName());
+        model.addAttribute("mainPage", mainPageResponseDto);
 
         return "/mainPage";
     }
+
 
     @GetMapping("/loginCheck")
     public String loginCheck(Model model, @CurrentSecurityContext(expression = "authentication") Authentication authentication) {
